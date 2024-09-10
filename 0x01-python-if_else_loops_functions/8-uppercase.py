bd3db0ae2dedcab1c6bb337c534fcd409908dc4e
M-sharkawy <mostafasharks021@gmail.com>
2024-09-11 00:11:10 +0300

    a function that convert to upper cases

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+
+    for character in str:
+        if ord(character) >= 97 and ord(character) <= 122:
+            character = ord(character) - 32
+            print("{}".format(chr(character)), end="")
+        else:
+            print("{}".format(character), end="")
