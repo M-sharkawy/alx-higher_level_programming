255ff8ed63fd29809bdd90d9934cf0de0c332d7d
M-sharkawy <mostafasharks021@gmail.com>
2024-09-11 01:01:14 +0300

    a function to print fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
         if num % 5 == 0 and num % 3 == 0:
             print("Fizz", end=" ")
         elif num % 5 == 0:
-            print("Buzz", end=" ")
+            print("Buzz", end="")
         elif num % 3 == 0:
             print("FizzBuzz", end="")
         else:
