c8461ec36204bf0b0505d3658bcf2f230b31ec5b
M-sharkawy <mostafasharks021@gmail.com>
2024-09-13 02:00:45 +0300

    program that imports functions from the file calculator_1.py, does some Maths,

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     """the sum, difference, multiple and quotient"""
-    from calculator_1.py import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
