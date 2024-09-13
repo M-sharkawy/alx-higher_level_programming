49ca0f8de894a64fe60e729b17144471db69dabc
M-sharkawy <mostafasharks021@gmail.com>
2024-09-13 04:06:12 +0300

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,11 +8,11 @@ def print_arg(argv):
         print("{:d} argument:".format(n))
     else:
         print("{:d} arguments:".format(n))
-    
     i = 1
     while i <= n:
         print("{:d}: {:s}".format(i, argv[i]))
         i += 1
+
 
 if __name__ == "__main__":
     import sys
