f7a2bf03f99b9f8e25ed3fd3dd60d5fd16b6ec56
M-sharkawy <mostafasharks021@gmail.com>
2024-09-13 04:32:20 +0300

    print sum of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-def add(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        sum = 0
-        while i <= n:
-            sum += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
+import sys
 
 if __name__ == "__main__":
-    import sys
-    add(sys.argv)
+    args = sys.argv[1:]
+    sum = 0
+    for arg in args:
+        sum += int(arg)
+    print(sum)
