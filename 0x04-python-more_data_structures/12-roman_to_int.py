55a4b6abd567c655a307f8e37dec99d2e1860a88
M-sharkawy <mostafasharks021@gmail.com>
2024-09-19 00:19:06 +0300

    a function that returns a list with all values multiplied by a number without using any loops.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or Type(roman_string) != str:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-
     nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    summ = 0
-
-    for index in range(len(roman_string)):
-        value = nums[roman_string[index]]
-        if index + 1 < len(roman_string) and nums[roman_string[index + 1]] > value:
-            summ -= value
+    total = 0
+    for idx in range(len(roman_string)):
+        value = nums[roman_string[idx]]
+        if idx + 1 < len(roman_string) and nums[roman_string[idx + 1]] > value:
+            total -= value
         else:
-            summ += value
-    return summ
+            total += value
+    return total
