e75f18e9c62c3cf6531fe531c452dc6d67c65467
M-sharkawy <mostafasharks021@gmail.com>
2024-10-01 21:06:36 +0300

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -54,15 +54,15 @@ class Square:
                         ...
             Args:
             -----
-                value (int) : suppose to be int value
+                value (int) : the square size
             Raises:
             -------
                 TypeError: if value is not an int
                 ValueError: if value is less than 0
             """
-            if type(size) is not int:
+            if type(value) is not int:
                 raise TypeError("size must be an integer")
-            elif size < 0:
+            elif value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
 
