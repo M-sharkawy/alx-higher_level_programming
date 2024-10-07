124d7fe0e0ba27533611c07ca4f346313cd22b8e
M-sharkawy <mostafasharks021@gmail.com>
2024-10-07 21:46:44 +0300

    add methods to the class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """this module Write an empty class Rectangle that defines a rectangle:"""
 
 
@@ -32,6 +31,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """height setter for width retrieval"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -46,6 +46,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """height setter for height retrieval"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
