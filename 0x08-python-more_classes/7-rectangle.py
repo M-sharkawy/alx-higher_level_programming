51accf3aecc8a0cdd9b57d0fb8923ea948777d8a
M-sharkawy <mostafasharks021@gmail.com>
2024-10-07 23:47:25 +0300

    class attribute print_symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -84,7 +84,7 @@ class Rectangle:
             return line
         for x in range(self.__height):
             for y in range(self.__width):
-                line += Rectangle.print_symbol
+                line += str(self.print_symbol)
             if x != self.__height - 1:
                 line += "\n"
         return line
