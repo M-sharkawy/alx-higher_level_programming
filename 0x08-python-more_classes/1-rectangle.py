2f4a73a85d06351b82a1a41c0cef079731151d0e
M-sharkawy <mostafasharks021@gmail.com>
2024-10-07 21:13:58 +0300

    form class of rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,13 +19,13 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """retrieve the width of rectangle"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -34,12 +34,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self._width = value
+            self.__width = value
 
     @property
     def height(self):
         """reterieve the height of the triangle"""
-        return self._width
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -48,4 +48,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self._height = value
+            self.__height = value
