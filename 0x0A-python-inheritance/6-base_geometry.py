3d58fac0ee4f7c4645190d1a46ca6981f9f40f95
M-sharkawy <mostafasharks021@gmail.com>
2024-10-14 16:51:23 +0300

    area exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""module containing an empty 'BaseGeometry' class"""
+
+
+class BaseGeometry:
+    """Base geometry class """
+    def area(self):
+        """calculates area"""
+        raise Exception("area() is not implemented")
