a701e1feb35ddb9f5557c0f6185c82df38cab12b
M-sharkawy <mostafasharks021@gmail.com>
2024-10-14 14:58:01 +0300

    function return all the attr. and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module to return all the attribute of an objects """
+
+
+def lookup(obj):
+    """function return all the attr. and methods of an object"""
+    return(dir(obj))
