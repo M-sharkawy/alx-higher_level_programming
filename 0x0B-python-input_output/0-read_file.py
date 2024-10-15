386c383da5b05d88966b362c53cf779bb3c1b60c
M-sharkawy <mostafasharks021@gmail.com>
2024-10-15 20:02:24 +0300

    function to read UTF8 txt file STOUT

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,4 @@
 def read_file(filename=""):
     """function reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="UTF-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.readlines())
