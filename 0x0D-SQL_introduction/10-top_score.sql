485ef9240f0b883c78c12147ae52d8a56e88ea39
M-sharkawy <mostafasharks021@gmail.com>
2024-11-19 23:26:49 +0200

    top score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table
+-- should display both the score and the name and ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
