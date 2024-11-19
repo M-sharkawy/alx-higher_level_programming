521d054b1fc1c578457255739995d45632221ca0
M-sharkawy <mostafasharks021@gmail.com>
2024-11-20 00:06:53 +0200

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,5 @@
 -- lists the number of records with the same score in the table second_table
-the number of records for this score with the label number
-The list should be sorted by the number of records (descending)
-SELECT COUNT(score) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
+GROUP BY score
 ORDER BY number DESC;
