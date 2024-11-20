baac9814b5d93b4995f8f1e7d87b924f5bf969c4
M-sharkawy <mostafasharks021@gmail.com>
2024-11-21 00:25:13 +0200

    6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE if NOT EXISTS hbtn_0d_usa;
-CREATE TABLE if NOT EXISTS states (
+CREATE TABLE if NOT EXISTS hbtn_0d_usa.states (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id),
