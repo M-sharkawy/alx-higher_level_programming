9ce106a9246fff989baccd570b65dc2c6a32a47f
M-sharkawy <mostafasharks021@gmail.com>
2024-11-21 01:03:35 +0200

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,5 @@
 --  lists all cities contained in the database hbtn_0d_usa
-SELECT
-        c.id,
-        c.name AS cities,
-        s.name AS states
-FROM
-    cities c
+SELECT c.id, c.name AS cities, s.name AS states
+FROM cities c
 LEFT JOIN states s ON c.state_id = s.id
 ORDER BY c.id ASC;
