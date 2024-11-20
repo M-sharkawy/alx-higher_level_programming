1ae8651917b3cade4ea8da7aca95bc3dacf5ef5a
M-sharkawy <mostafasharks021@gmail.com>
2024-11-21 00:16:29 +0200

    5

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creates the table id_not_null
-CREATE TABLE if NOT EXISTS id_not_null(
-    id INT DEFAULT = 1,
+CREATE TABLE if NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
     name VARCHAR(256)
 );
