2226da9c2eca3e2b7d8ca6c10d428c7f3a7ef01e
M-sharkawy <mostafasharks021@gmail.com>
2024-12-17 01:48:50 +0200

    6 tasks

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arr_strings = ["C is fun", "Python is cool", "JavaScript is amazing"]
+const length_ = arr_strings.length
+let ind = 0;
+
+for (ind; ind < length_; ind++)
+{
+    console.log(arr_strings[ind]);
+}
