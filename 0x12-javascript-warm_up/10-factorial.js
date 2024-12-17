db574fe1e062d9f16da71cb3e1ed0c0cb28b28d6
M-sharkawy <mostafasharks021@gmail.com>
2024-12-17 23:11:07 +0200

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+
+function factorial(num) {
+    if (num <= 0 || isNaN(num))
+        return 1;
+
+    return num * factorial(num - 1);
+}
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0], 10);
+console.log(factorial(num));
