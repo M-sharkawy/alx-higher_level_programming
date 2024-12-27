3a2c3f69c727a2daf082d50751592e6cfe55c5b2
M-sharkawy <mostafasharks021@gmail.com>
2024-12-27 21:45:45 +0200

    task 2: Filter states by format

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module that takes in an argument and displays all values 
+in the states table of hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+import sys
+
+"""Connect to the database"""
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    """Create a cursor object"""
+    cursor = db.cursor()
+
+    order = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY states.id;".format(sys.argv[4])
+    cursor.execute(order)
+
+    [print(row) for row in cursor.fetchall()]
+
+    cursor.close()
+    db.close()
