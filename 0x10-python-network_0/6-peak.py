bd811eb248d23101e0d7d585447dce181c1719b3
M-sharkawy <mostafasharks021@gmail.com>
2025-01-17 01:33:33 +0200

    peak detection in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''module to detect the peak number in an list'''
 
+
 def find_peak(list_of_integers):
     if list_of_integers is None:
         return None
@@ -8,7 +9,7 @@ def find_peak(list_of_integers):
     max_no = None
 
     for nums in list_of_integers:
-        if max_no == None or max_no < nums:
+        if max_no is None or max_no < nums:
             max_no = nums
 
     return max_no
