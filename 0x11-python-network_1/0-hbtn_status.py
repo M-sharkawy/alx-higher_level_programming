b91dccbb97f42a8d5bb6398e1043cd277d21bb22
M-sharkawy <mostafasharks021@gmail.com>
2025-01-18 01:18:38 +0200

    hbtn_status file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this module used Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+from urllib.request import urlopen
+
+with urlopen("https://alx-intranet.hbtn.io/status", ) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
