c229ce8132f01fd1ebff2e519ef306a06ed80cca
M-sharkawy <mostafasharks021@gmail.com>
2025-01-18 18:30:20 +0200

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that send POST request to the passed URL with
+the email as a parameter, and finally displays the body of the response"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    email = {'email' : sys.argv[2]}
+    response = requests.post(sys.argv[1], params=email)
+    print(response.text)
+    response.close()
