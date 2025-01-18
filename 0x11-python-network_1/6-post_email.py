993d104f658c66e239d9033cf26b329a39aabcd3
M-sharkawy <mostafasharks021@gmail.com>
2025-01-18 18:32:38 +0200

    task 6 edits

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,6 @@ import sys
 
 if __name__ == "__main__":
 
-    email = {'email' : sys.argv[2]}
+    email = {'email': sys.argv[2]}
     response = requests.post(sys.argv[1], data=email)
     print(response.text)
-    response.close()
