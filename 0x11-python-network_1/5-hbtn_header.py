95042c464fdeb3cf42170f420a91f333e454750a
M-sharkawy <mostafasharks021@gmail.com>
2025-01-18 18:17:24 +0200

    headers by requests lib

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that  displays the value of the variable
+X-Request-Id in the response header"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    response = requests.get(sys.argv[1])
+    print(response.headers["X-Request-Id"])
+    response.close()
